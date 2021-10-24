05f47b6b5afed0293449e4ab1e2c5d09b354d4e1
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2021-10-24 17:51:41 -0300

    added hateoas links to getAllUsers

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -22,6 +22,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
@@ -31,12 +34,22 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec, @PageableDefault( page = 0,
+    public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec, @PageableDefault(page = 0,
             size = 10,
             sort = "userId",
             direction = Sort.Direction.ASC) Pageable pageable) {
 
         Page<UserModel> userModelPage = userService.findAll(pageable, spec);
+
+        if (!userModelPage.isEmpty()) {
+            userModelPage.forEach(user -> user.add(
+                            linkTo(
+                                    methodOn(UserController.class)
+                                            .getOneUser(user.getUserId())
+                            ).withSelfRel()
+                    )
+            );
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
