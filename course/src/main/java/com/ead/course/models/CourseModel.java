ee9ac73ca73a744f182d42d492d488a9cf8995dd
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2021-10-28 13:00:00 -0300

    added delete logic

--- course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -12,7 +12,6 @@ import org.hibernate.annotations.FetchMode;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
