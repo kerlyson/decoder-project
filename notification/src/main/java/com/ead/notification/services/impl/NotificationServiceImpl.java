f98792dc4b69c6dfe4a015189b80363db90efe53
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2022-04-10 10:40:54 -0300

    Criar Endpoints para visualizar e atualizar Notifications

--- notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,10 +1,16 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.enums.NotificationStatus;
 import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRespository;
 import com.ead.notification.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -20,5 +26,19 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public void saveNotification(NotificationModel notificationModel) {
         notificationRespository.save(notificationModel);
+    }
+
+    @Override
+    public Page<NotificationModel> findAllNotificationByUser(UUID userId, Pageable pageable) {
+        return notificationRespository.findAllByUserIdAndNotificationStatus(
+                userId,
+                NotificationStatus.CREATED,
+                pageable
+        );
+    }
+
+    @Override
+    public Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId) {
+        return notificationRespository.findByNotificationIdAndUserId(notificationId, userId);
     }
 }
